4235b23b140df7ee152b7fbbe4bfd73cf2ba2340
BradleyHazel <bradleyhazel@gmail.com>
2022-07-01 09:38:41 -0400

    Css add

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Sidebar from './Sidebar'
 function Main() {
   return (
-    <div>Main
+    <div className="container">
         <Sidebar />
-        <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+        <main className="flex-1 ml-40">
+            <div  className="flex h-screen w-screen p-20 bg-gradient-to-tr from-slate-500 to-green-300 ">
+                <h1 className="text-4xl">Top Content</h1>
+            </div>
+        </main>
     </div>
   )
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div>
+         <div class="flex">
+        <aside class="w-40 fixed left-0 top-0 h-screen bg-slate-700 p-10 content-center">
+            <img src="https://cdn.iconscout.com/icon/free/png-256/knight-63-904196.png"/>
+            <button className="
+            bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white  hover:border-transparent rounded
+            fixed left-1 px-12 bottom-3 py-2
+            ">Logout</button>
+            
+        </aside>
+        </div>
+    </div>
   )
 }
 
