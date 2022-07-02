52656e0dfe312ccc08ff4f82ece887780dc9953d
BradleyHazel <bradleyhazel@gmail.com>
2022-07-02 16:47:02 -0400

    Side bar fully responsive

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,12 +3,19 @@ import React from 'react'
 function Sidebar() {
   return (
     <div >
-   <aside className="w-14 sm:w-20 md:w-24 lg:w-28 xl:w-34 2xl:w-40    h-screen bg-slate-600  content-center">
-            <img src="https://cdn.iconscout.com/icon/free/png-256/knight-63-904196.png"/>
-            <div class='flex flex-col '>
-            <button style={{position:"relative"}} class=" bg-blue-500 hover:bg-blue-700 text-white font-bold border border-blue-700 rounded">
-              logout
-            </button>
+   <aside className="flex justify-between	 flex-col w-14 sm:w-20 md:w-24 lg:w-28 xl:w-34 2xl:w-40    h-screen bg-slate-600  content-center">
+            <aside  className="flex flex-col">
+              <img src="https://cdn.iconscout.com/icon/free/png-256/knight-63-904196.png"/>
+              <div class='flex flex-col'>
+              <button style={{}} class=" bg-blue-500 hover:bg-blue-700 text-white font-bold border border-blue-700 rounded">
+                Add 
+              </button>
+            </div>
+            </aside>
+            <div class='flex flex-col'>
+              <button style={{}} class=" bg-blue-500 hover:bg-blue-700 text-white font-bold border border-blue-700 rounded">
+                logout
+              </button>
             </div>
             
         </aside>
