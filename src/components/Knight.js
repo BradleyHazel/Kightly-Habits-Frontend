8374998a0653a49bddfba6a47ab2759fcd0878f1
BradleyHazel <bradleyhazel@gmail.com>
2022-07-02 18:18:48 -0400

    Responsive knights

--- src/components/Knight.js
+++ src/components/Knight.js
@@ -3,18 +3,16 @@ import React from 'react'
 
 function Knight() {
   return (
-    <aside class="p-10">  
+    <aside class="p-2 sm:p-4 md:p-4 lg:p-6 w-5/6 sm:w-1/2 md:w-2/5 lg:w-1/4 xl:w-1/4 2xl:w-1/6 m-5">  
       <div  style={{ display:"flex", flexDirection:"column", justifyContent:"space-evenly"}} className="max-w-sm rounded overflow-hidden shadow-lg">
-
-        <img src={'https://opengameart.org/sites/default/files/BronzeKnight.gif'} />
-
-      <div class="px-6 py-4" style={{background: "red"}}>
-        <div class="font-bold text-xl mb-2">Mountain</div>
-        <p class="text-gray-700 text-base">
+        <img  src={'https://opengameart.org/sites/default/files/BronzeKnight.gif'} />
+        <div class="px-6 py-4" style={{background: "red"}}>
+          <div class="font-bold text-xl mb-2">Bronze Habit Knight</div>
+          <p class="text-gray-700 text-base">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
-        </p>
-      </div>
-      <div class="px-6 pt-4 pb-2">
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Sidebar() {
   return (
     <div >
-   <aside className="flex justify-between	 flex-col w-14 sm:w-20 md:w-24 lg:w-28 xl:w-34 2xl:w-40    h-screen bg-slate-600  content-center">
+   <aside className="flex justify-between	 flex-col w-20 sm:w-20 md:w-24 lg:w-28 xl:w-34 2xl:w-40    h-screen bg-slate-600  content-center">
             <aside  className="flex flex-col">
               <img src="https://cdn.iconscout.com/icon/free/png-256/knight-63-904196.png"/>
               <div class='flex flex-col'>
